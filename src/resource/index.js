69f9828f3c1522612f66776745487409d8cb16e0
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2019-02-25 18:12:38 -0300

    Correcciones Dylan. Se agrega view CrudCalendars

--- src/resource/index.js
+++ src/resource/index.js
@@ -1,12 +1,12 @@
 import AuthProvider from './providers/AuthProvider'
-import CalendarProvider from './providers/CalendarProvider'
-import AppointmentProvider from './providers/AppointmentProvider'
+import CalendarProviderClass from './providers/CalendarProvider'
+import AppointmentProviderClass from './providers/AppointmentProvider'
 
 
 // Give arg to provider to start endpoint with specific path for example = xxx.com/api/person
 export const AuthService = new AuthProvider('auth')
 
-export const CalendarProvider = new CalendarProvider('/zfmc/api/calendars')
+export const CalendarProvider = new CalendarProviderClass('/zfmc/api/calendars')
 
 
-export const AppointmentProvider = new AppointmentProvider('/zfmc/api/appointments')
+export const AppointmentProvider = new AppointmentProviderClass('/zfmc/api/appointments')
