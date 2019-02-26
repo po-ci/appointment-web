042cfb6eb395b9d1b3da7a09ef2fa6d57de455ae
Dylan <dy199869@gmail.com>
2019-02-26 17:53:35 -0300

    Calendars Table

--- src/components/CalendarsTable.vue
+++ src/components/CalendarsTable.vue
@@ -7,6 +7,11 @@
             Calendarios
           </v-card-title>
           <v-card-text>
+            <v-data-table
+              :headers="headers"
+              :items="desserts">
+
+            </v-data-table>
             <p v-for="item of getCalendars">{{item.name}}</p>
           </v-card-text>
 
@@ -26,14 +31,30 @@
   export default {
     name: "CalendarsTable",
     data: () => ({
-      select: null
+      select: null,
+      headers: [
+        {
+          text: '',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        {text: 'ID', value: 'id'},
+        {text: 'Nombre', value: 'nombre'},
+        {text: 'Usuario', value: 'usuario'},
+        {text: 'Aciones', value: 'acciones', sortable: false},
+      ],
+      desserts: [],
+
     }),
     computed: {
       ...mapGetters([
         'getCalendars']),
     },
     methods: {
-      ...mapActions(['fetchCalendars'])
+      ...mapActions(['fetchCalendars']),
+
+
     },
     created() {
       this.fetchCalendars()
