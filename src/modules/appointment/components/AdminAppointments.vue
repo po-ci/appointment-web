810e1cdfc7034f65fa8cc47ad6a3aa21947581ff
Dylan <dy199869@gmail.com>
2019-03-26 11:12:23 -0300

    Se avanza con la vista de administracion de turnos

--- src/modules/appointment/components/AdminAppointments.vue
+++ src/modules/appointment/components/AdminAppointments.vue
@@ -13,7 +13,23 @@
 
         </v-card>
       </v-flex>
-      <v-flex class="pa-2" xs12 sm12 md12>
+      <v-flex class="pa-2 offset-md2" xs12 md8>
+        <v-card class="elevation-4">
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs12 sm12 md12 class="px-4 py-1">
+                <appointments-calendar-selected></appointments-calendar-selected>
+              </v-flex>
+              <v-flex xs12 sm12 md12 class="px-4 py-1">
+                <admin-appointments-date-picker></admin-appointments-date-picker>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex class="pa-2 offset-md2" xs12 md8>
         <v-card class="elevation-4">
           <v-tabs fixed-tabs v-model="tabs">
             <v-tab :key="'appointments'" ripple>Turnos</v-tab>
@@ -27,7 +43,7 @@
     <v-tabs-items v-model="tabs">
       <v-tab-item :key="'appointments'">
         <v-layout row wrap>
-          <v-flex xs12 class="pa-2">
+          <v-flex xs12 md8 class="pa-2 offset-md2">
             <v-card>
               <v-card-text>Todos los turnos</v-card-text>
             </v-card>
@@ -36,134 +52,18 @@
       </v-tab-item>
 
 
-      <v-tab-item :key="'assing'">
+      <v-tab-item :key="'assing'" class="pa-2">
         <v-layout row wrap>
-          <v-flex xs12 sm12 md4 class="pa-2">
-            <v-card class="appointment-step-card">
-              <v-card-title class="pa-1">
-                <h3 class="pa-0">
-                  <v-btn fab dark small color="primary">
-                    1
-                  </v-btn>
-                  Elegir Agenda
-                </h3>
-              </v-card-title>
-
-              <v-card-text class="text-xs-center pa-1">
-                <appointments-calendars></appointments-calendars>
+          <v-flex xs12 md8 class="offset-md2">
+            <v-card>
+              <v-card-text>
+                <admin-appointments-hours></admin-appointments-hours>
               </v-card-text>
             </v-card>
           </v-flex>
-
-          <v-flex xs12 sm12 md4 class="pa-2">
-            <v-card class="appointment-step-card">
-              <v-card-title class="pa-1">
-                <h3 class="pa-0">
-                  <v-btn fab dark small color="primary">
-                    2
-                  </v-btn>
-                  Selecionar Dia
-                </h3>
-              </v-card-title>
-
-              <v-card-text class="text-xs-center pa-1">
-
-                <div v-if="isAgendaSelected" class="text-xs-center">
-                  <v-alert
-                    class="ma-5 title"
-                    :value="true"
-                    color="info"
-                    outline
-                  >
-                    Seleccionar agenda
-                  </v-alert>
-
-                </div>
-
-                <appointments-date-picker v-else></appointments-date-picker>
-              </v-card-text>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 sm12 md4 class="pa-2">
-            <v-card class="appointment-step-card">
-              <v-card-title class="pa-1">
-                <h3 class="pa-0">
-                  <v-btn fab dark small color="primary">
-                    3
-                  </v-btn>
-                  Confirmar Horario
-                </h3>
-              </v-card-title>
-
-              <v-card-text class="pa-1">
-
-                <div v-if="isAgendaAndDaySelected" class="text-xs-center">
-                  <v-alert
-                    class="ma-5 title"
-                    :value="true"
-                    color="info"
-                    outline
-                  >
-                    Seleccionar agenda y dia
-                  </v-alert>
-
-                </div>
-
-                <div v-else-if="getCalendarLoading" class="text-xs-center mt-5">
-                  <span class="primary--text headline">Buscando turnos disponibles</span><br>
-                  <v-progress-circular
-                    class="mt-3"
-                    :size="100"
-                    :width="6"
-                    color="primary"
-                    indeterminate
-                  ></v-progress-circular>
-                </div>
-
-                <div v-else-if="getAvailableShifts.length > 0">
-
-                  <!--          <v-layout row wrap class="mb-1 mt-0">
-                              <v-flex class="text-xs-center">
-                                <h5 class="grey&#45;&#45;text text&#45;&#45;darken-1 title text-uppercase pa-0">
-                                  <v-icon>calendar_today</v-icon>
-                                  {{getCalendarSelected.name}}: {{getFriendlyDateFormated}}
-                                </h5>
-                              </v-flex>
-                            </v-layout>-->
-
-                  <div style="height:280px; overflow-y: scroll; overflow-x: hidden;">
-                    <!--<appointments-available v-for="s in getAvailableShifts"-->
-                    <!--:date="getDateFormated"-->
-                    <!--:day="getDateDay"-->
-                    <!--:start="s.start"-->
-                    <!--:hour="s.hour"-->
-                    <!--:duration="s.duration"-->
-                    <!--:calendar="getCalendarSelected"-->
-                    <!--v-on:bookAppointment="bookAppointment"-->
-                    <!--&gt;</appointments-available>-->
-                  </div>
-                </div>
-
-
-                <div v-else>
-                  <v-alert
-                    class=" headline text-xs-center"
-                    :value="true"
-                    color="warning"
-                    icon="priority_high"
-                    outline
-                  >
-                    No hay turnos disponibles para la fecha seleccionada. <br>Intenta otra fecha por favor.
-                  </v-alert>
-                </div>
-
-
-              </v-card-text>
-            </v-card>
-          </v-flex>
-
         </v-layout>
+
+
       </v-tab-item>
     </v-tabs-items>
   </v-container>
@@ -171,14 +71,18 @@
 
 <script>
   import AppointmentsCalendars from './AppointmentsCalendarRadio'
-  import AppointmentsDatePicker from './AppointmentsDatePicker'
+  import AppointmentsCalendarSelected from './AppointmentsCalendarSelect'
+  import AdminAppointmentsDatePicker from './AdminAppointmentsDatePicker'
+  import AdminAppointmentsHours from './AdminAppointmentsHours'
   import {mapActions, mapGetters} from 'vuex'
 
   export default {
     name: "AdminAppointments",
     components: {
       AppointmentsCalendars,
-      AppointmentsDatePicker
+      AppointmentsCalendarSelected,
+      AdminAppointmentsDatePicker,
+      AdminAppointmentsHours
     },
     data: () => ({
         dialog: false,
@@ -202,10 +106,6 @@
 
     },
     computed: {
-
-      isAgendaSelected() {
-        return (this.getCalendarSelected) ? false : true
-      },
       isAgendaAndDaySelected() {
         return (this.getDate && this.getCalendarSelected) ? false : true
       },
@@ -225,13 +125,6 @@
         this.setCalendarSelected(null)
         this.$store.commit('SET_DATE', null)
       },
-      bookAppointment: function (appointmentData) {
-
-        this.clearLastAppointment()
-        this.dialog = true
-        this.appointment = appointmentData
-      },
-
       ...mapActions([
         'fetchCalendars',
         'fetchAvailableAppointments',
---
+++ src/modules/appointment/components/AdminAppointmentsHours.vue
@@ -0,0 +1,108 @@
+<template>
+  <div v-if="isAgendaAndDaySelected" class="text-xs-center">
+    <v-alert
+      class="ma-5 title"
+      :value="true"
+      color="info"
+      outline
+    >
+      Seleccionar agenda y dia
+    </v-alert>
+
+  </div>
+
+  <div v-else-if="getCalendarLoading" class="text-xs-center mt-5">
+    <span class="primary--text headline">Buscando turnos disponibles</span><br>
+    <v-progress-circular
+      class="mt-3"
+      :size="100"
+      :width="6"
+      color="primary"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+
+  <div v-else-if="getAvailableShifts.length > 0">
+
+    <!--          <v-layout row wrap class="mb-1 mt-0">
+                <v-flex class="text-xs-center">
+                  <h5 class="grey&#45;&#45;text text&#45;&#45;darken-1 title text-uppercase pa-0">
+                    <v-icon>calendar_today</v-icon>
+                    {{getCalendarSelected.name}}: {{getFriendlyDateFormated}}
+                  </h5>
+                </v-flex>
+              </v-layout>-->
+
+    <div style="height:330px; overflow-y: scroll; overflow-x: hidden;">
+      <appointments-available v-for="s in getAvailableShifts"
+                              :date="getDateFormated"
+                              :day="getDateDay"
+                              :start="s.start"
+                              :hour="s.hour"
+                              :duration="s.duration"
+                              :calendar="getCalendarSelected"
+                              v-on:bookAppointment="bookAppointment"
+      ></appointments-available>
+    </div>
+  </div>
+
+
+  <div v-else>
+    <v-alert
+      class=" headline text-xs-center"
+      :value="true"
+      color="warning"
+      icon="priority_high"
+      outline
+    >
+      No hay turnos disponibles para la fecha seleccionada. <br>Intenta otra fecha por favor.
+    </v-alert>
+  </div>
+</template>
+
+<script>
+  import AppointmentsAvailable from './AppointmentsAvailable'
+  import {mapActions, mapGetters} from 'vuex'
+
+  export default {
+    name: "AdminAppointmentsHours",
+    components: {
+      AppointmentsAvailable
+    },
+    computed: {
+      ...mapGetters([
+        'getDateFormated',
+        'getDateDay',
+        'getDate',
+        'getCalendarSelected',
+        'getAvailableShifts',
+        'getCalendarLoading',
+        'getFriendlyDateFormated'
+      ]),
+      isAgendaSelected() {
+        return (this.getCalendarSelected) ? false : true
+      },
+      isAgendaAndDaySelected() {
+        return (this.getDate && this.getCalendarSelected) ? false : true
+      },
+    },
+    methods: {
+      ...mapActions([
+        'fetchCalendars',
+        'fetchAvailableAppointments',
+        'clearLastAppointment',
+        'setCalendarSelected'
+      ]),
+      bookAppointment: function (appointmentData) {
+
+        this.clearLastAppointment()
+        this.dialog = true
+        this.appointment = appointmentData
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
