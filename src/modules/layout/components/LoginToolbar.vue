cac1bd6524de93be0598c220e0007cf3409eda3b
cristian.cdi@gmail.com <Neverwinter2>
2019-03-28 19:50:45 -0300

    Se resuelve Tareas #115. Se agrega un setinterval que verifica el tiempo de login

--- src/modules/layout/components/LoginToolbar.vue
+++ src/modules/layout/components/LoginToolbar.vue
@@ -1,38 +1,45 @@
 <template>
 
-    <avatar-toolbar v-if="isLogin"></avatar-toolbar>
-    <v-btn v-else
-           flat
-           dark
-           to="/login"
-    >
-        Iniciar Sesión
-    </v-btn>
+  <avatar-toolbar v-if="isLogin"></avatar-toolbar>
+  <v-btn v-else
+         flat
+         dark
+         to="/login"
+  >
+    Iniciar Sesión
+  </v-btn>
 
 </template>
 
 <script>
-    import {mapGetters,mapActions} from 'vuex'
-    import AvatarToolbar from './AvatarToolbar'
+  import {mapGetters, mapActions} from 'vuex'
+  import AvatarToolbar from './AvatarToolbar'
 
-    export default {
-        name: "LoginToolbar",
-        components: {AvatarToolbar},
-        computed: {
-            ...mapGetters([
-                'getUser',
-                'isLogin'
-            ]),
-        },
-        mounted: function(){
-            this.checkAuth()
-        },
-        methods: {
-            ...mapActions([
-                'checkAuth'
-            ])
-        }
+  export default {
+    name: "LoginToolbar",
+    components: {AvatarToolbar},
+    computed: {
+      ...mapGetters([
+        'getUser',
+        'isLogin'
+      ]),
+    },
+    mounted: function () {
+      this.checkAuth()
+      this.controlLoginExpire()
+    },
+    methods: {
+      controlLoginExpire: function () {
+        const self = this;
+        setInterval(function(){
+          self.checkAuth()
+        },60000)
+      },
+      ...mapActions([
+        'checkAuth'
+      ])
     }
+  }
 </script>
 
 <style scoped>
