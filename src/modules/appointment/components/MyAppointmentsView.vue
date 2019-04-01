c3802d9d6ed36a3861efb11dae07e332c77357e8
Dylan <dy199869@gmail.com>
2019-04-01 12:18:21 -0300

    Se mejora dialogo cancelar turno

--- src/modules/appointment/components/MyAppointmentsView.vue
+++ src/modules/appointment/components/MyAppointmentsView.vue
@@ -6,46 +6,15 @@
         <v-btn v-if="appointment.status == 1"
                class="ml-3 mb-0 px-1"
                :color="'amber lighten-4'"
-               @click="dialogCancel = true"
+               @click="$emit('dialogCancel', appointment )"
                small
 
         >
-         Cancelar
+          Cancelar
         </v-btn>
       </v-card-text>
 
     </v-card>
-    <v-dialog
-      v-model="dialogCancel"
-      max-width="500"
-    >
-      <v-card>
-        <v-card-title class="title" primary-title>
-          Desea cancelar este turno?
-        </v-card-title>
-        <v-card-text>
-          Si cancela el turno no se podrá recuperar nuevamente, y en tal caso deberá solicitar uno nuevo.
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="grey"
-            flat="flat"
-            @click="dialogCancel = false"
-          >
-            Cancelar
-          </v-btn>
-          <v-btn
-            color="green darken-1"
-            flat="flat"
-            :loading="getCalendarLoading"
-            @click="appointmentCancel(appointment.id)"
-          >
-            Aceptar
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
@@ -66,13 +35,13 @@
     },
     computed: {
       ...mapGetters([
-        'getCalendarLoading'
+        'getAppointmentsLoading', 'getResultAppointment'
       ]),
-      getClass: function(){
-        if(this.appointment.status == 2 || this.appointment.status == 3){
+      getClass: function () {
+        if (this.appointment.status == 2 || this.appointment.status == 3) {
           return 'border-error'
         }
-        if(this.appointment.status == 1){
+        if (this.appointment.status == 1) {
           return 'border-green'
         }
         return ''
@@ -84,18 +53,18 @@
       ]),
       appointmentCancel(appointmentId) {
         this.cancelAppointment(appointmentId)
-        this.dialogCancel = this.getCalendarLoading
       }
 
-    }
+    },
   }
 </script>
 
 <style scoped>
-.border-error{
-  border:1px solid red;
-}
-.border-green{
-  border:1px solid green;
-}
+  .border-error {
+    border: 1px solid red;
+  }
+
+  .border-green {
+    border: 1px solid green;
+  }
 </style>
